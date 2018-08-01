4a334c5eb2d713d9239c0dc274a781c67c7ca89c
unknown <t.bakeer4@gmail.com>
2018-07-31 23:32:00 -0700

    fixed server.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,14 @@
 var db = require("../models");
+//require express 
+var express = require("express");
 
-module.exports = function(app) {
+//use routers
+var router = express.Router();
+
+// router.get("/", function (req, res) {
 
 
-  // Get all examples
-//   app.get("/api/examples", function(req, res) {
-//     db.Example.findAll({}).then(function(dbExamples) {
-//       res.json(dbExamples);
-//     });
-//   });
+// })
 
-//   // Create a new example
-//   app.post("/api/examples", function(req, res) {
-//     db.Example.create(req.body).then(function(dbExample) {
-//       res.json(dbExample);
-//     });
-//   });
 
-//   // Delete an example by id
-//   app.delete("/api/examples/:id", function(req, res) {
-//     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-//       res.json(dbExample);
-//     });
-//   });
-}
+ 
