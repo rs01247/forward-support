8d96d928cd220987b384defe62b015d19411ba0e
Raymond <rs01247@gmail.com>
2018-08-01 23:03:24 -0700

    CRUD for service tickets, also added port on config.json

--- controller/ticket.controller.js
+++ controller/ticket.controller.js
@@ -3,18 +3,53 @@ var express = require("express");
 //use routers
 var router = express.Router();
 
-
-router.post('/api/user', function (req, res) {
-    // get the body request and added to database 
-
-
+//POST REQUEST TO SAVE A NEW SERVICE TICKET AND ADD TO DB
+router.post("/api/user", function (req, res) {
+    db.Ticket.create({
+        employeeName: req.body.employeeName,
+        summary: req.body.summary,
+        employeeDepartment: req.body.employeeDepartment,
+        priority: req.body.priority,
+        ticketCategory: req.body.ticketCategory,
+        status: req.body.status
+    }).then(function (dbTicket) {
+        res.json(dbTicket);
+    })
 });
 
-router.get('/api/user/', function (req, res) {
+
+router.get("/api/user/", function (req, res) {
     // return the tickets that are not closed from database to the user 
-
-
+    db.Ticket.findAll({}).then(function (dbTicket) {
+        res.json(dbTicket);
+    })
 });
+
+// UPDATE STATUS LEVEL FOR SERVICE TICKET
+router.put("api/user", function (req, res) {
+    db.Ticket.update({
+        status: req.body.status
+    }, {
+        where: {
+            id: req.body.id
+        }
+    }).then(function(dbTicket) {
+        res.json(dbTicket);
+    })
+})
+
+// UPDATE PRIOIRTY LEVEL (ESCALATION)
+router.put("api/user", function (req, res) {
+    db.Ticket.update({
+        priority: req.body.priority
+    }, {
+        where: {
+            id: req.body.id
+        }
+    }).then(function(dbTicket) {
+        res.json(dbTicket);
+    })
+})
 
 router.get('/', function (req, res) {
     res.render('index', {
