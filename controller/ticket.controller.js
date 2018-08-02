514f9c25cc79759652f811212d23b667fae1593d
unknown <t.bakeer4@gmail.com>
2018-08-02 15:46:35 -0700

    added handlebars files and test user get and post tickets

--- controller/ticket.controller.js
+++ controller/ticket.controller.js
@@ -5,23 +5,43 @@ var router = express.Router();
 
 //POST REQUEST TO SAVE A NEW SERVICE TICKET AND ADD TO DB
 router.post("/api/user", function (req, res) {
+    //get data from req.body 
+    var data = req.body;
+    var employeeName = data.employeeName;
+    var employeeDepartment = data.employeeDepartment;
+    var summary = data.summary;
+    var category = data.category;
+    var priority = data.ticketCategory;
+    var status = "open";
+    //validate if any of the data are empty then res.send(0)
+    if (employeeName === "" || employeeDepartment ==="" || summary ==="" || category ==="" || priority ==="") {
+        console.log("empty")
+      //do something here to tell the user there is an emplty data 
+    }
+else{
     db.Ticket.create({
         employeeName: req.body.employeeName,
         summary: req.body.summary,
         employeeDepartment: req.body.employeeDepartment,
         priority: req.body.priority,
         ticketCategory: req.body.ticketCategory,
-        status: req.body.status
+        status: status
     }).then(function (dbTicket) {
-        res.json(dbTicket);
+       res.redirect("/user")
+      
     })
+    // add catch error
+}
 });
 
 
-router.get("/api/user/", function (req, res) {
+router.get("/user", function (req, res) {
     // return the tickets that are not closed from database to the user 
-    db.Ticket.findAll({}).then(function (dbTicket) {
-        res.json(dbTicket);
+    db.Ticket.findAll({ where: {
+        status:"open"
+        //condition to show only the user tickets not all tickets
+      }}).then(function (dbTicket) {
+      res.render("user",{Ticket:dbTicket})
     })
 });
 
@@ -30,38 +50,13 @@ router.put("api/user", function (req, res) {
     db.Ticket.update({
         status: req.body.status
     }, {
-        where: {
-            id: req.body.id
-        }
-    }).then(function(dbTicket) {
-        res.json(dbTicket);
-    })
+            where: {
+                id: req.body.id
+            }
+        }).then(function (dbTicket) {
+            res.json(dbTicket);
+        })
 })
-
-// UPDATE PRIOIRTY LEVEL (ESCALATION)
-router.put("api/user", function (req, res) {
-    db.Ticket.update({
-        priority: req.body.priority
-    }, {
-        where: {
-            id: req.body.id
-        }
-    }).then(function(dbTicket) {
-        res.json(dbTicket);
-    })
-})
-
-router.get('/', function (req, res) {
-    res.render('index', {
-        title: 'Support Ticket System'
-    })
-});
-
-router.get('/user', function (req, res) {
-    res.render('user', {
-        title: 'Support Ticket System'
-    })
-});
 
 
 router.get('/admin', function (req, res) {
@@ -69,6 +64,28 @@ router.get('/admin', function (req, res) {
         title: 'Support Ticket System'
     })
 });
+
+
+// UPDATE PRIOIRTY LEVEL (ESCALATION)
+router.put("api/user", function (req, res) {
+    db.Ticket.update({
+        priority: req.body.priority
+    }, {
+            where: {
+                id: req.body.id
+            }
+        }).then(function (dbTicket) {
+            res.json(dbTicket);
+        })
+})
+
+router.get('/index', function (req, res) {
+    res.render('index', {
+        title: 'Support Ticket System'
+    })
+});
+
+
 
 
 module.exports = router;
--- models/ticket.js
+++ models/ticket.js
@@ -24,7 +24,7 @@ module.exports = function (sequelize, DataTypes) {
         },
         status:
         {
-            type: DataTypes.INTEGER,
+            type: DataTypes.STRING,
             allowNull: false
         }
 
