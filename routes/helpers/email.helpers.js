77721ed6a04d5ab8a1d824f2f9818d62742cb173
Raymond <rs01247@gmail.com>
2018-08-10 11:57:22 -0700

    updated title and some formatting

--- routes/helpers/email.helpers.js
+++ routes/helpers/email.helpers.js
@@ -15,14 +15,14 @@ const emailer = {
         html: '<strong>Your ticket is now in progress!!! We will update you as soon as a solution is found. -Forward Support</strong>',
     },
     completeMail: {
-        //to: '',
+        to: '',
         from: 'forwardsup1247@gmail.com',
         subject: 'Your Ticket Has Been Completed!',
         text: 'We have provide you with a solution to your problem! If your issue is resolved, please close your ticket. If it is not resolved, please contact us again. -Forward Support',
         html: '<strong>We have provide you with a solution to your problem. If your issue is resolved, please close your ticket. If it is not resolved, please contact us again. -Forward Support</strong>',
     }, 
     closeMail: {
-        //to: '',
+        to: '',
         from: 'forwardsup1247@gmail.com',
         subject: 'Your Ticket is Now Closed',
         text: 'Your ticket has been closed. Thank you so much for contacting us. If any other issues arise, do not hesitate to contact us again. -Forward Support',
